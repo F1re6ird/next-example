a26f7134aefa0d449073b74ecf0fdf1773a65ed4
Yekini Abdulwahab <104094403+F1re6ird@users.noreply.github.com>
2025-05-24 19:08:37 +0100

    fitst commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Invoice = () => {
+    return (
+        <p>Invoices Pages</p>
+    )
+}
+
+export default Invoice
---
+++ app/ui/font.ts
@@ -0,0 +1,13 @@
+import { Inter, Roboto, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter',
+    display: 'swap',
+});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    variable: '--font-lusitana'
+})
